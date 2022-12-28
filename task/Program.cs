e5db3b7363388b3b3c9ba2bf39d1b1a6606e74d4
maxim chernokhvoy <buguevmaxim@gmail.com>
2022-12-28 22:30:07 +0500

    add task-code

---
+++ task/Program.cs
@@ -0,0 +1,21 @@
+﻿void RealeseArray(string[] array)
+{
+    string newArray = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            if (newArray == string.Empty)
+                newArray = array[i];
+            else
+                newArray += " " + array[i];
+    }
+    string[] resultArray = newArray.Split(" ").ToArray();
+    Console.WriteLine($"[{string.Join(", ", resultArray]");
+}
+
+Console.Clear();
+string[] array = Console.ReadLine().Split(" ").ToArray();
+Console.WriteLine("Исходный массив:");
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine("Итоговый массив:");
+RealeseArray(array);
